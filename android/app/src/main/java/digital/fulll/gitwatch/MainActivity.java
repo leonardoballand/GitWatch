624055818f2da79c2144d1d8c60f3ee3f7c30fdb
Leonardo Balland <l.balland@inextenso.digital>
2022-05-23 14:38:29 +0200

    fix migration

--- android/app/src/main/java/digital/fulll/gitwatch/MainActivity.java
+++ android/app/src/main/java/digital/fulll/gitwatch/MainActivity.java
@@ -1,6 +1,8 @@
 package digital.fulll.gitwatch;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
 import com.facebook.react.ReactActivityDelegate; // <- add this necessary import
 import com.zoontek.rnbootsplash.RNBootSplash; // <- add this necessary import
 import com.zoontek.rnbars.RNBars; // <- add this necessary import
@@ -16,16 +18,43 @@ public class MainActivity extends ReactActivity {
     return "GitWatch";
   }
 
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * you can specify the rendered you wish to use (Fabric or the older renderer).
+   */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-
-      @Override
-      protected void loadApp(String appKey) {
-        RNBootSplash.init(MainActivity.this); // <- initialize the splash screen
-        super.loadApp(appKey);
-        RNBars.init(MainActivity.this, "dark-content"); // <- initialize with initial bars styles (could be light-content)
-      }
-    };
+    return new MainActivityDelegate(this, getMainComponentName());
   }
+  public static class MainActivityDelegate extends ReactActivityDelegate {
+    public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
+      super(activity, mainComponentName);
+    }
+    @Override
+    protected ReactRootView createRootView() {
+      ReactRootView reactRootView = new ReactRootView(getContext());
+      // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+      reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+      return reactRootView;
+    }
+
+    @Override
+    protected void loadApp(String appKey) {
+      RNBootSplash.init(getPlainActivity()); // <- initialize the splash screen
+      super.loadApp(appKey);
+    }
+  }
+
+  // @Override
+  // protected ReactActivityDelegate createReactActivityDelegate() {
+  //   return new ReactActivityDelegate(this, getMainComponentName()) {
+
+  //     @Override
+  //     protected void loadApp(String appKey) {
+  //       RNBootSplash.init(MainActivity.this); // <- initialize the splash screen
+  //       super.loadApp(appKey);
+  //       RNBars.init(MainActivity.this, "dark-content"); // <- initialize with initial bars styles (could be light-content)
+  //     }
+  //   };
+  // }
 }
